f2c857b0836b195c3413db2c13025f149a26d7df
kennethhas <kennethHasiholan@gmail.com>
2024-07-31 23:23:07 -0400

    commit

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -34,7 +34,7 @@ function LoginScreen({ navigation }) {
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button title="Login" onPress={handleLogin} />
+      <Button title="kenneth" onPress={handleLogin} />
       <TouchableOpacity onPress={() => navigation.navigate('Registration')}>
         <Text style={styles.registrationLink}>Don't have an account? Register</Text>
       </TouchableOpacity>
